9b91836e44f61c1b1eb5713e99e3597ab2b010d2
Vadim <vadimkomerzan84@gmail.com>
2022-12-19 13:01:02 +0200

    work

--- home_3.py
+++ home_3.py
@@ -5,11 +5,9 @@ c = "book"
 print(id(a))
 print(id(b))
 print(id(c))
-
 print("-" * 50)
-
-a = ["book"]
-b = ["book"]
+a = ["book_test"]
+b = ["book_test"]
 
 print(id(a))
 print(id(b))
@@ -17,3 +15,24 @@ print(id(b))
 print("-" * 50)
 
 print(type(a), (type(b)), (type(c)))
+
+
+print("-" * 50)
+
+a = ["book"]
+b = ["book"]
+c = ["book"]
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+print(type(a), (type(b)), (type(c)))
+
+a = "book_test"
+b = "book_test"
+
+print(id(a))
+print(id(b))
+
+print(type(a), (type(b)))
