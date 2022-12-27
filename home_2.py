c1f0c714ce276cc13c184dbe46a5bef12b1369c5
Vadim <vadimkomerzan84@gmail.com>
2022-12-27 23:43:56 +0200

    work

--- home_2.py
+++ home_2.py
@@ -5,4 +5,25 @@ def print_max(a, b, ):
         print(b, "maximum")
 
 
-print_max(9, 6)
+print_max(2, 6)
+
+
+
+def min_x(c, d, v):
+    if c < d and v > c:
+        print(c, "minimum")
+    elif d < v:
+        print(d, "minimum")
+    else:
+        print(v, "minimum")
+
+
+min_x(3, 1, 5)
+
+
+def amount(z, x):
+    s = z + x
+    return s
+
+
+print(amount(2, 3))
